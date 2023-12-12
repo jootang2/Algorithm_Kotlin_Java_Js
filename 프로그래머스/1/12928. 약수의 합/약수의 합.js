39bca8cb123c942c475162b82aa569b62984ccc4
jootang2 <106504552+jootang2@users.noreply.github.com>
2023-12-12 10:44:07 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer;
+    let sum = 0;
+    
+    for(let i = 1; i <= n; i ++){
+        if(n % i === 0){
+            sum += i;
+        }
+    }
+    
+    answer = sum;
+    return answer;
+}
